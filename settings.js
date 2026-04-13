db6a1dc28dccef229236929725386238a74c0d06
GUIVARRA <cherywidson331@gmail.com>
2026-04-12 21:04:46 -0400

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'BAHINAVA-MD-V1',
+  author: '‎',
+  botName: "bahinava MD",
+  botOwner: 'ASUNAX', // Your name
+  ownerNumber: '8297010740', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+  updateZipUrl: "https://github.com/GUIVARRA/BAHINAVA/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
